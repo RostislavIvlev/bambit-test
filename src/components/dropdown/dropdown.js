e254b301fba5c2c0e5d2ce7e16576b6c508ef842
RostislavIvlev <56726375+RostislavIvlev@users.noreply.github.com>
2021-10-30 14:01:47 +0500

    dropdown квадратный бордер при клике

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -3,10 +3,14 @@ import $ from "jquery";
 global.jQuery = $;
 global.$ = $;
 
+
 $(function() {
-    $(".dropdown-btn").click(function() {
+    $(".dropdown-btn").on("click",(function() {
         $(this).next(".dropdown").slideToggle();
-    })
+        if ($(this).hasClass("dropdown-btn_toggled")) {
+            $(this).removeClass("dropdown-btn_toggled")
+        } else $(this).addClass("dropdown-btn_toggled")
+    }))
 })
 
 $(document).on("click", (function(e) {
@@ -14,6 +18,7 @@ $(document).on("click", (function(e) {
     let saleText = "";
     if (!$(target).is(".dropdown-btn") && !$(target).parents().is(".dropdown-btn")) {
         $(".dropdown").slideUp();
+        $(".dropdown-btn").removeClass("dropdown-btn_toggled")
     }
     
     if ($(target).is(".dropdown__item")) {
